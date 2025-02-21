eb2653c87aa9519d403c37f7ddf0f02c824ca167
Angel <riyapuri0710@gmail.com>
2025-02-21 19:29:51 +0530

    21-02-2025 Classwork

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 
+//middleware to parse urlencoded data
+express.urlencoded({ extended: true });
+let userdata = []
+
+
 app.get("/signup", (req, res) => {
-    res.sendFile("./signup.html");
+    res.sendFile(__dirname+"/signup.html");
 })
 
 app.post("/signup", (req, res) => {
     let{username, useremail, userpassword} = req.body;
-    console.log(username, useremail, userpassword);
+    let newUser ={
+        name: username, email: useremail, password: userpassword
+    }
+    userdata.push(newUser);
+    // console.log(username, useremail, userpassword);
 })
 
 app.listen(3434,()=>{
